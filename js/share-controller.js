4770eb8fc9c85f62e6649509b373bd13b24620c5
BenErnst <benernst3@gmail.com>
2021-11-01 01:02:23 +0200

    responsive check

--- js/share-controller.js
+++ js/share-controller.js
@@ -5,16 +5,29 @@
 function uploadImg() {
     cleanLineRect();
 
+
     const imgDataUrl = gCanvas.toDataURL("image/jpeg");
 
     // A function to be called if request succeeds
     function onSuccess(uploadedImgUrl) {
         const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl);
 
+        showModal();
+
         document.querySelector('.facebook-box').innerHTML = `
-        <a class="btn share-btn" href="https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
-        <img class="facebook-logo" src="icons/facebook-logo.png">
-        </a>`
+        <a href="https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
+        <img class="social-logo" src="icons/facebook-logo.png">
+        </a>`;
+
+        document.querySelector('.whatsapp-box').innerHTML = `
+        <a href="https://api.whatsapp.com/send?text=${encodedUploadedImgUrl}" title="Share on Whatsapp" target="_blank" onclick="window.open('https://api.whatsapp.com/send?text=${uploadedImgUrl}'); return false;">
+        <img class="social-logo" src="icons/whatsapp-logo.png">
+        </a>`;
+
+        document.querySelector('.twitter-box').innerHTML = `
+        <a href="https://twitter.com/intent/tweet?url=${encodedUploadedImgUrl}" title="Share on Twitter" target="_blank" onclick="window.open('https://twitter.com/intent/tweet?url=${uploadedImgUrl}'); return false;">
+        <img class="social-logo" src="icons/twitter-logo.png">
+        </a>`;
     }
     doUploadImg(imgDataUrl, onSuccess);
 }
@@ -39,31 +52,38 @@ function doUploadImg(imgDataUrl, onSuccess) {
 }
 //
 
-function initWebShareApi() {
-    const elShareBtn = document.querySelector('.share-btn');
+function showModal() {
     const elOverlay = document.querySelector('.overlay');
     const elShareModal = document.querySelector('.share-modal');
+    // elOverlay.classList.add('show-share-modal');
+    elOverlay.style.visibility = 'visible';
+    elShareModal.classList.add('show-share-modal');
+}
+
+function initWebShareApi() {
+    const elWebApiBtn = document.querySelector('.web-api-btn');
+    const elOverlay = document.querySelector('.overlay');
+    // const elShareModal = document.querySelector('.share-modal');
 
     const title = window.document.title;
     var url = window.document.location.href;
 
-    elShareBtn.addEventListener('click', () => {
+    elWebApiBtn.addEventListener('click', () => {
         if (navigator.share) {
             navigator.share({
                 title: `${title}`,
                 url: `${url}`
             })
         }
-        // else {
-        //     elOverlay.classList.add('show-share-modal');
-        //     elShareModal.classList.add('show-share-modal');
-        // }
     })
 
-    elOverlay.addEventListener('click', () => {
-        elOverlay.classList.remove('show-share-modal');
-        elShareModal.classList.remove('show-share-modal');
-    })
+    elOverlay.addEventListener('click', () => { closeModal() })
 }
 
-
+function closeModal() {
+    const elOverlay = document.querySelector('.overlay');
+    const elShareModal = document.querySelector('.share-modal');
+    // elOverlay.classList.add('show-share-modal');
+    elOverlay.style.visibility = 'hidden';
+    elShareModal.classList.remove('show-share-modal');
+}
